db47873ab035b3a64529a84821c100a85a7b4abd
matthew <uiwwsw@icloud.com>
2022-02-05 12:16:31 +0900

    ddd

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ enum ASSET_TYPE {
   IMAGE,
 }
 interface State {
-  interface?: boolean;
+  inventory?: boolean;
   setting?: boolean;
   gameover?: boolean;
-  wordStep: number[];
+  line: number;
+  cursor: number;
+  propmt: number;
   chapter: number;
   scene: number;
+  sentence: number;
 }
 interface Choice {
   question: Sentence;
@@ -22,10 +25,8 @@ interface Charactor {
 }
 interface Sentence {
   word: string;
-  wait?: number;
-  wordStep?: number;
+  promptSpeed?: number;
   audio?: string;
-  enter?: boolean;
 }
 // type Prompt = (Sentence[] | Charactor | Choice)[];
 // interface Prompt {
@@ -65,6 +66,8 @@ interface Chapter {
   assets: Assets[];
 }
 class Canvas {
+  protected pause = false;
+  protected freese = false;
   protected width = 0;
   protected height = 0;
   #canvas = document.getElementById("visual-novel") as HTMLCanvasElement;
@@ -73,10 +76,9 @@ class Canvas {
     this.resizing(800, 600);
   }
 
-  protected requestAnimationFrame() {
-    this.draw();
-    // this.requestAnimationFrame();
-    requestAnimationFrame(this.requestAnimationFrame.bind(this));
+  protected startFrame() {
+    if (!this.freese) this.draw();
+    requestAnimationFrame(() => this.startFrame());
   }
   protected resizing(width: number, height: number) {
     this.width = width;
@@ -107,7 +109,7 @@ class Canvas {
 // }
 class Game extends Canvas {
   // class Game extends Dom {
-  readonly wordStep = 1 / 5;
+  readonly promptSpeed = 1 / 5;
   protected chapter: Chapter[];
   protected assets: Id<Assets[]>;
   protected state: State;
@@ -129,7 +131,7 @@ class Game extends Canvas {
         return { ...a, ...v };
       }, {});
     addEventListener("keypress", this.addEventListener.bind(this));
-    this.requestAnimationFrame();
+    this.startFrame();
 
     // this.assets = [{url}]
   }
@@ -139,13 +141,27 @@ class Game extends Canvas {
     this.state = {
       chapter: 0,
       scene: 0,
-      wordStep: [0],
+      propmt: 0,
+      sentence: 0,
+      line: 0,
+      cursor: 0,
     };
   }
   addEventListener(e: KeyboardEvent) {
     switch (e.key) {
       case "i":
-        this.state.interface = !this.state.interface;
+        this.pause = !this.pause;
+        this.state.inventory = !this.state.inventory;
+        break;
+      case "Enter":
+        if (this.state.line >= this.isSentence.length) {
+          this.state.propmt += 1;
+          this.state.cursor = 0;
+          this.state.line = 0;
+        } else {
+          this.state.line += 1;
+        }
+        // this.state.promptSpeed = [0];
         break;
     }
   }
@@ -154,70 +170,131 @@ class Game extends Canvas {
 
   render() {}
 
-  interface() {
-    if (this.state?.interface) {
+  drawInventory() {
+    if (this.state?.inventory) {
       this.context.fillStyle = "black";
       this.context.fillRect(0, 0, this.width, this.height);
     }
   }
-  scene() {
+
+  get isSentence() {
+    return this.chapter[this.state.chapter].scene[this.state.scene].prompt[
+      this.state.propmt
+    ].sentence;
+  }
+  drawScene() {
+    // const width = 600;
+    // const height = 200;
+    // const promptSpeed = this.isSentence.promptSpeed || this.promptSpeed;
+    // this.context.fillStyle = "black";
+    // this.context.fillRect(0, this.height - height, this.width, height);
+    // this.context.font = "48px serif";
+    // this.context.fillStyle = "white";
+    // for (const _index in this.state.promptSpeed) {
+    //   const index = +_index;
+    //   const startIndex = this.state.promptSpeed[index - 1] || 0;
+    //   const endIndex = this.state.promptSpeed[index];
+    //   const word = this.isSentence.word.substring(startIndex, endIndex);
+    //   this.context.fillText(
+    //     word,
+    //     (this.width - width) / 2,
+    //     this.height - height + 100 + index * 50,
+    //     width
+    //   );
+    //   if (index === this.state.promptSpeed.length - 1) {
+    //     if (this.state.promptSpeed[index] > this.isSentence.word.length) {
+    //       break;
+    //     }
+    //     const nextWord = this.isSentence.word.substring(
+    //       startIndex,
+    //       endIndex + promptSpeed
+    //     );
+    //     if (this.context.measureText(nextWord).width > width) {
+    //       this.state.promptSpeed.push(this.state.promptSpeed[index]);
+    //       // console.log("dawdawdawdawdawd");
+    //     } else {
+    //       this.state.promptSpeed[index] += promptSpeed;
+    //     }
+    //   }
+    // }
+  }
+
+  drawPrompt() {
     const width = 600;
     const height = 200;
-    const word = this.chapter[0].scene[0].prompt[0].sentence[0].word;
-    const wordStep =
-      this.chapter[0].scene[0].prompt[0].sentence[0].wordStep || this.wordStep;
+
     this.context.fillStyle = "black";
     this.context.fillRect(0, this.height - height, this.width, height);
     this.context.font = "48px serif";
     this.context.fillStyle = "white";
-    for (const _index in this.state.wordStep) {
+    for (const _index in this.isSentence) {
       const index = +_index;
-      const startIndex = this.state.wordStep[index - 1] || 0;
-      const endIndex = this.state.wordStep[index];
-      // if (index > 2) break;
+      const promptSpeed =
+        this.isSentence[index].promptSpeed || this.promptSpeed;
+      let word = this.isSentence[index].word;
+
+      if (this.state.line < index) break;
+      if (this.state.line === index) {
+        word = word.substring(0, this.state.cursor);
+      }
+      this.state.cursor += promptSpeed;
       this.context.fillText(
-        word.substring(startIndex, endIndex),
+        word,
         (this.width - width) / 2,
         this.height - height + 100 + index * 50,
         width
       );
-      // this.state.wordStep[+index] += wordStep;
-      if (index === this.state.wordStep.length - 1)
-        if (
-          this.context.measureText(
-            word.substring(startIndex, endIndex + wordStep)
-          ).width > width
-        ) {
-          this.state.wordStep.push(this.state.wordStep[+index]);
-        } else {
-          this.state.wordStep[+index] += wordStep;
-        }
+      if (word === this.isSentence[index].word && this.state.line <= index) {
+        this.state.line += 1;
+        this.state.cursor = 0;
+      }
+      // const startIndex = this.state.line < index
+      // const endIndex = this.state.promptSpeed;
+      // const word = this.isSentence.word.substring(startIndex, endIndex);
+      // this.context.fillText(
+      //   word,
+      //   (this.width - width) / 2,
+      //   this.height - height + 100 + index * 50,
+      //   width
+      // );
+      // // console.log(this.isSentence.word[index], index);
+      // // console.log("=============");
+      // // console.log(word);
+      // if (index === this.state.promptSpeed.length - 1) {
+      //   if (word === this.isSentence.word[index]) {
+      //     if (this.isSentence.word.length > this.state.promptSpeed.length) {
+      //       this.state.promptSpeed.push(0);
+      //     }
+      //   } else {
+      //     this.state.promptSpeed[index] += promptSpeed;
+      //   }
+      // }
+
+      // if (index === this.state.promptSpeed.length - 1) {
+      //   if (this.state.promptSpeed[index] > this.isSentence.word.length) {
+      //     break;
+      //   }
+      //   const nextWord = this.isSentence.word[index].substring(
+      //     startIndex,
+      //     endIndex + promptSpeed
+      //   );
+      //   if (this.context.measureText(nextWord).width > width) {
+      //     this.state.promptSpeed.push(0);
+      //     // console.log("dawdawdawdawdawd");
+      //   } else {
+      //     this.state.promptSpeed[index] += promptSpeed;
+      //   }
+      // }
     }
-    // if (
-    //   this.context.measureText(word.substring(0, this.isWordStep)).width > width
-    // ) {
-    // }
-    // while (line <= this.isWordLine) {
-    //   if (
-    //     this.context.measureText(word.substring(0, this.isWordStep)).width >
-    //     width
-    //   ) {
-    //     this.isWordLine = 1;
-    //   }
-    //   this.context.fillText(
-    //     word.substring(0, this.isWordStep),
-    //     (this.width - width) / 2,
-    //     this.height - height + 100 + line * 50,
-    //     width
-    //   );
-    // }
   }
 
   draw() {
     super.draw();
-    // console.log("djlakwdaw", this);
-    this.scene();
-    this.interface();
+    if (!this.pause) {
+      this.drawScene();
+      this.drawPrompt();
+    }
+    this.drawInventory();
   }
 }
 const dd = new Game([
@@ -231,7 +308,26 @@ const dd = new Game([
               images: {},
             },
             sentence: [
-              { word: "테스트세트스으 rkske 가나다 간다ㅏ... 아저앚마앚" },
+              {
+                word: "안녕하세요",
+              },
+              {
+                word: "반가워요",
+              },
+            ],
+          },
+          {
+            charactor: {
+              name: "matthew",
+              images: {},
+            },
+            sentence: [
+              {
+                word: "안녕하세요",
+              },
+              {
+                word: "반가워요",
+              },
             ],
           },
         ],
