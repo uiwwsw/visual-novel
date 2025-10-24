b1d937cec2f87b3a87dd05d5092e0d1c8939945c
matthew <uiwwsw@icloud.com>
2025-10-24 09:24:07 +0900

    feat: raise node version and optimize fade animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,48 +4,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      keyframes: {
-        'fade--in': {
-          '0%': {
-            opacity: 0,
-          },
-          '100%': {
-            opacity: 1,
-          },
-        },
-        'fade--stay': {
-          '0%': {
-            opacity: 1,
-          },
-          '100%': {
-            opacity: 1,
-          },
-        },
-        'fade--out': {
-          '0%': {
-            opacity: 1,
-          },
-          '100%': {
-            opacity: 0,
-          },
-        },
-        'fade--change': {
-          '0%': {
-            opacity: 0,
-          },
-          '100%': {
-            opacity: 0,
-          },
-        },
-      },
-      animation: {
-        'fade--in': 'fade--in 1000ms',
-        'fade--stay': 'fade--stay 1000ms',
-        'fade--out': 'fade--out 1000ms',
-        'fade--change': 'fade--change 1ms',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
