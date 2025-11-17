c0c56605de6640901fba01e8886cd181c7fa1ac7
matthew <uiwwsw@icloud.com>
2025-11-17 17:10:36 +0900

    Tweak battle layout and start menu positioning

--- src/pages/StartMenu.tsx
+++ src/pages/StartMenu.tsx
@@ -26,7 +26,7 @@ const StartMenuPage = () => {
       <audio src={asset.audio} autoPlay />
       <div className="relative h-full text-white">
         <img className="h-full w-full object-contain" src={asset.image} alt="시작화면" />
-        <div className="pointer-events-none absolute inset-0 flex flex-col items-end justify-end gap-4 p-6">
+        <div className="pointer-events-none absolute inset-0 flex flex-col items-end justify-start gap-4 p-6">
           <div className="pointer-events-auto w-full max-w-xl rounded-3xl border border-white/10 bg-black/70 p-5 text-sm backdrop-blur">
             <div className="flex flex-col gap-3">
               <Btn autoFocus onClick={handleStart}>
