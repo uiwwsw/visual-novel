9428972531df32127ca0e982c7023fa03f4a4fb8
uiwwsw <uiwwsw@icloud.com>
2026-02-24 14:13:44 +0900

    init

--- src/components/useStorageContext.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { StorageContext } from './StorageContextContext';
-
-export const useStorageContext = () => useContext(StorageContext);
--- src/utils/getJson.ts
+++
@@ -1,4 +0,0 @@
-export const getJson = async <T>(fileName: string) => {
-  const res = await fetch(`/${fileName}.json?t=${Date.now()}`);
-  return await res.json() as T;
-};
